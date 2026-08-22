8b921a71abe4825669d1826bc523664b27f22738
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 14:56:28 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='accountBalance'
+    name='accounting'
+    title='accountingAccounts'
+    {...props}
+/>
