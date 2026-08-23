a7955fc90ff1211a8c00d3197f853c8db550d8b6
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 14:51:23 +0330

    committed by script

--- site/parts/accountSummary.jsx
+++ site/parts/accountSummary.jsx
@@ -1,4 +1,4 @@
-export default ({ account }) => <dl class='accountSummary'>
+export default ({ account }) => <dl class='summary'>
     <dt class='title'>{account?.title}</dt>
     <dd class='code'>{account?.code}</dd>
     <dd class='type'>{account?.accountType}</dd>
